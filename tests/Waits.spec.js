2ea9f27b66c0fede4cc36d1c861fa34ca310fe11
Humairaanjum0510 <humaira.anjum@delaplex.com>
2026-05-07 15:58:26 +0530

    .,

--- tests/Waits.spec.js
+++ tests/Waits.spec.js
@@ -10,5 +10,9 @@ test("ClientApp", async ({ page }) => {
   //await page.waitForLoadState("networkidle");
   //waitFor() only works for single element
   await productTitles.first().waitFor();
-  console.log(await productTitles.allTextContents());
+  console.log(await productTitles.allTextContents());\
+  console.log("Test completed");
+    console.log("Test completed");
+
 });
+
